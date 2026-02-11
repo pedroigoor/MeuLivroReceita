4313b2d12f9d4a6dca6181fcf82ead5a0c6e9040
Pedro almeida <pedro.igor10@live.com>
2026-02-10 22:58:48 -0300

    feat: adding

---
+++ src/Shared/MyRecipeBook.Communication/Resopnses/ResponseErrorJson.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRecipeBook.Communication.Resopnses
+{
+    public class ResponseErrorJson
+    {
+        public IList<string> Erros {  get; }
+      
+        public ResponseErrorJson(IList<string> erros) {
+            Erros = erros; ;
+        }
+
+        public ResponseErrorJson(string erros)
+        {
+            Erros = [erros];
+        }
+    }
+}
