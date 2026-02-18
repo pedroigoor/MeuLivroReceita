460c89af0665e1cff04d692c163950f79a320a97
Pedro almeida <pedro.igor10@live.com>
2026-02-18 17:12:32 -0300

    Feat: adding entity user

---
+++ src/Backend/MyRecipeBook.Domain/Entities/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRecipeBook.Domain.Entities
+{
+    public class EntityBase 
+    {
+        public long Id { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public bool Active { get; set; } = true;
+    }
+}
---
+++ src/Backend/MyRecipeBook.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRecipeBook.Domain.Entities
+{
+    public class User : EntityBase
+    {
+      
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+     
+       
+    }
+}
