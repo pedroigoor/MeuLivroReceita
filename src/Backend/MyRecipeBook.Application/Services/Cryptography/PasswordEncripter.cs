87f6024814b39073c003871e525582b08d65bc98
Pedro almeida <pedro.igor10@live.com>
2026-02-21 23:13:40 -0300

    teste sonar

--- src/Backend/MyRecipeBook.Application/Services/Cryptography/PasswordEncripter.cs
+++ src/Backend/MyRecipeBook.Application/Services/Cryptography/PasswordEncripter.cs
@@ -7,7 +7,7 @@ namespace MyRecipeBook.Application.Services.Cryptography
     {
         public static string Encrypt(string password)
         {
-            // Implementação de criptografia de senha (exemplo usando SHA256)
+            // Implementação de criptografia de senha (exemplo usando SHA256) rtese
             var bytes = Encoding.UTF8.GetBytes(password);
             var hash = SHA256.HashData(bytes);
             return Convert.ToBase64String(hash);
